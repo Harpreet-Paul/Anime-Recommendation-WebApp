08e4dc38a2a39f668b2d5af7af03ca9ce4852ebe
Harpreet-Paul <59809370+Harpreet-Paul@users.noreply.github.com>
2020-08-13 16:35:56 -0400

    Update ars_app.py
    set k and global_mean as global variables
    updated parameters to make_recommendations function based on changes to make_recommendations.py
    loading joblib similarity_matrix and iid_rid_array objects from gcs

--- ars_app.py
+++ ars_app.py
@@ -1,44 +1,57 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
-from joblib import load
+
 import pickle
 import pandas as pd
 import flask
 import make_recommendations
 import create_gcs_local_file
+from joblib import load, dump
 
 
-# In[ ]:
+# In[2]:
+
+
+k = 20
+
+
+# In[3]:
+
+
+global_mean = 7.328450933497362   
+
+
+# In[4]:
 
 
 bucket_name = "anime-recommendation-system"
 
 
-# In[ ]:
+# In[5]:
 
 
 create_gcs_local_file.create_gcs_local_file(bucket_name, "saved-objects/pkl-objects/ids_biases_array.pkl", "local_ids_biases_array.pkl")
 ids_biases_array = pickle.load(open("local_ids_biases_array.pkl", "rb"))
 
 
-# In[ ]:
-
-
-create_gcs_local_file.create_gcs_local_file(bucket_name, "saved-objects/pkl-objects/joblib_prediction_algo", "local_joblib_prediction_algo")
-prediction_algo = load("local_joblib_prediction_algo")
-
-
-# In[ ]:
+# In[6]:
 
 
 create_gcs_local_file.create_gcs_local_file(bucket_name, "saved-objects/pkl-objects/joblib_similarity_matrix", "local_joblib_similarity_matrix")
 similarity_matrix = load("local_joblib_similarity_matrix")
 
 
-# In[ ]:
+# In[7]:
+
+
+create_gcs_local_file.create_gcs_local_file(bucket_name, "saved-objects/pkl-objects/joblib_iid_rid_array", "local_joblib_iid_rid_array")
+iid_rid_array = load("local_joblib_iid_rid_array")
+
+
+# In[8]:
 
 
 create_gcs_local_file.create_gcs_local_file(bucket_name, "saved-objects/anime_list/AnimeList.csv", "local_anime_list.csv")
@@ -62,7 +75,7 @@ def main():
         username = flask.request.form['username']
         num_of_recommendations = int(flask.request.form['number of recommendations'])
         
-        recommendations = make_recommendations.make_recommendations(username, ids_biases_array, similarity_matrix, prediction_algo, num_of_recommendations, anime_list_df)
+        recommendations = make_recommendations.make_recommendations(username, ids_biases_array, similarity_matrix, global_mean, iid_rid_array, k, num_of_recommendations, anime_list_df)
         
     return flask.render_template('main',
                                      original_input={'Username':username,
@@ -72,4 +85,12 @@ def main():
 
 if __name__ == '__main__':
     app.run()
+
+
+# In[ ]:
+
+
+
+
+
 
